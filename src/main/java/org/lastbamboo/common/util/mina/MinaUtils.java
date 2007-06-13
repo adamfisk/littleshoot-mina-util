ecd3a1b2304b6304b5e1838a284a3508db84f6e8
adamfisk <a@littleshoot.org>
2007-06-13 18:05:32 +0000

    Added splitting of buffers to arrays
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/util/mina@518 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/util/mina/MinaUtils.java
+++ src/main/java/org/lastbamboo/common/util/mina/MinaUtils.java
@@ -3,6 +3,8 @@ package org.lastbamboo.common.util.mina;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
+import java.util.Collection;
+import java.util.LinkedList;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.mina.common.ByteBuffer;
@@ -53,11 +55,93 @@ public class MinaUtils
      */
     public static byte[] toByteArray(final ByteBuffer buf)
         {
-        final byte[] bytes = new byte[buf.capacity()];
+        final byte[] bytes = new byte[buf.remaining()];
         buf.get(bytes);
         return bytes;
         }
+    
+    /**
+     * Splits the specified <code>ByteBuffer</code> into smaller 
+     * <code>ByteBuffer</code>s of the specified size.  The remaining bytes 
+     * in the buffer must be greater than the chunk size.  This method will
+     * create smaller buffers of the specified size until there are fewer 
+     * remaining bytes than the chunk size, when it will simply add a buffer
+     * the same size as the number of bytes remaining.
+     * 
+     * @param buffer The <code>ByteBuffer</code> to split.
+     * @param chunkSize The size of the smaller buffers to create.
+     * @return A <code>Collection</code> of <code>ByteBuffer</code>s of the
+     * specified size.  The final buffer in the <code>Collection</code> will
+     * have a size > 0 and <= the chunk size.
+     */
+    public static Collection<ByteBuffer> split(final ByteBuffer buffer, 
+        final int chunkSize)
+        {
+        final Collection<ByteBuffer> buffers = new LinkedList<ByteBuffer>();
+        final int originalLimit = buffer.limit();
+        
+        int totalSent = 0;
+        while ((totalSent + chunkSize) < originalLimit)
+            {
+            buffer.limit(totalSent + chunkSize);
+            buffers.add(createBuffer(buffer));            
+            totalSent += chunkSize;
+            }
+        
+        // Send any remaining bytes.
+        buffer.limit(originalLimit);
+        buffers.add(createBuffer(buffer));
+        return buffers;
+        }
+    
+    private static ByteBuffer createBuffer(final ByteBuffer buffer)
+        {
+        // We calculate this here because the final split buffer will not
+        // necessarily have a size equal to the chunk size -- it will
+        // usually be smaller.
+        final ByteBuffer data = ByteBuffer.allocate(
+            buffer.limit() - buffer.position());
+        
+        data.put(buffer);
+        data.flip();
+        return data;
+        }
 
+    /**
+     * Splits the specified <code>ByteBuffer</code> into smaller 
+     * <code>ByteBuffer</code>s of the specified size.  The remaining bytes 
+     * in the buffer must be greater than the chunk size.  This method will
+     * create smaller buffers of the specified size until there are fewer 
+     * remaining bytes than the chunk size, when it will simply add a buffer
+     * the same size as the number of bytes remaining.
+     * 
+     * @param buffer The <code>ByteBuffer</code> to split.
+     * @param chunkSize The size of the smaller buffers to create.
+     * @return A <code>Collection</code> of <code>ByteBuffer</code>s of the
+     * specified size.  The final buffer in the <code>Collection</code> will
+     * have a size > 0 and <= the chunk size.
+     */
+    public static Collection<byte[]> splitToByteArrays(final ByteBuffer buffer, 
+        final int chunkSize)
+        {
+        final Collection<byte[]> buffers = new LinkedList<byte[]>();
+        final int originalLimit = buffer.limit();
+        
+        int totalSent = 0;
+        while ((totalSent + chunkSize) < originalLimit)
+            {
+            buffer.limit(totalSent + chunkSize);
+            
+            // This will just read up to the limit.
+            buffers.add(toByteArray(buffer));            
+            totalSent += chunkSize;
+            }
+        
+        // Send any remaining bytes.
+        buffer.limit(originalLimit);
+        buffers.add(toByteArray(buffer));
+        return buffers;
+        }
 
     /**
      * Puts an unsigned byte into the buffer.
