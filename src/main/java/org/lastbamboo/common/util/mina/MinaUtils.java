4216471c43cf36891409abcd81956a814b7a3388
adamfisk <a@littleshoot.org>
2007-06-19 22:09:59 +0000

    Added method for creating a standard TCP acceptor
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/util/mina@621 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/util/mina/MinaUtils.java
+++ src/main/java/org/lastbamboo/common/util/mina/MinaUtils.java
@@ -5,9 +5,20 @@ import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.mina.common.ByteBuffer;
+import org.apache.mina.common.DefaultIoFilterChainBuilder;
+import org.apache.mina.common.IoServiceConfig;
+import org.apache.mina.common.IoServiceListener;
+import org.apache.mina.common.ThreadModel;
+import org.apache.mina.filter.codec.ProtocolCodecFactory;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.executor.ExecutorFilter;
+import org.apache.mina.transport.socket.nio.SocketAcceptor;
+import org.apache.mina.transport.socket.nio.SocketAcceptorConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -213,5 +224,43 @@ public class MinaUtils
         final long value)
         {
         bb.putInt(position, (int) (value & 0xffffffffL));
+        }
+
+    /**
+     * Utility method for creating a TCP acceptor with our default 
+     * configuration.
+     * 
+     * @param codecFactory The codec factory to use with the acceptor.
+     * @param ioServiceListener The listener for IO service events.
+     * @return The new acceptor.
+     */
+    public static SocketAcceptor createTcpAcceptor(
+        final ProtocolCodecFactory codecFactory, 
+        final IoServiceListener ioServiceListener)
+        {
+        final Executor threadPool = Executors.newCachedThreadPool();
+        final SocketAcceptor acceptor = new SocketAcceptor(
+            Runtime.getRuntime().availableProcessors() + 1, threadPool);
+        final IoServiceConfig acceptorConfig = acceptor.getDefaultConfig();
+        acceptorConfig.setThreadModel(ThreadModel.MANUAL);
+        
+        final SocketAcceptorConfig cfg = new SocketAcceptorConfig();
+        
+        // Just hoping this method does what it sounds like it does.
+        cfg.setDisconnectOnUnbind(true);
+        
+        // Not sure why we do this, but almost all the MINA examples set 
+        // acceptors to reuse the address.
+        cfg.getSessionConfig().setReuseAddress(true);
+        
+        acceptor.addListener(ioServiceListener);
+        
+        final DefaultIoFilterChainBuilder filterChainBuilder = 
+            cfg.getFilterChain();
+        filterChainBuilder.addLast("codec", 
+            new ProtocolCodecFilter(codecFactory));
+        filterChainBuilder.addLast("threadPool", 
+            new ExecutorFilter(Executors.newCachedThreadPool()));
+        return acceptor;
         }
     }
